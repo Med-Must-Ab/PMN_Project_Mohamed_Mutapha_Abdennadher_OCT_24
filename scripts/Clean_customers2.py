64f8037eb31560ead889731a34c48e2f3e093be9
Med-Must-Ab <141573842+Med-Must-Ab@users.noreply.github.com>
2024-10-18 18:43:22 +0200

    Update Clean_customers2.py

--- scripts/Clean_customers2.py
+++ scripts/Clean_customers2.py
@@ -12,11 +12,11 @@ df_customers = sqlContext.read.format("com.databricks.spark.csv") \
     .option("inferSchema", "true") \
     .load("hdfs://localhost/user/cloudera/raw_data/Project/Customers/")
 
-# Show the original data for inspection
+# Inspect Data
 print("Original Data:")
 df_customers.show(5, truncate=False)
 
-# Remove the first row if it contains the header information
+# Remove the first row if it contains the header
 header_row = df_customers.first()  # Get the first row
 df_customers = df_customers.filter(df_customers['C0'] != header_row['C0'])  # Filter out the header row
 
@@ -31,7 +31,7 @@ df_customers.show(5, truncate=False)
 # Duplicates and missing values cleaning
 df_customers_clean = df_customers.dropna(subset=["CustomerKey", "FirstName", "LastName"]).dropDuplicates(["CustomerKey"])
 
-# Debug: Show the data after cleaning to ensure it's not empty
+# Debug: Show the data after cleaning
 print("Data after cleaning:")
 df_customers_clean.show(5, truncate=False)
 print("Number of rows after cleaning: %d" % df_customers_clean.count())
@@ -42,5 +42,5 @@ df_customers.show(5, truncate=False)
 customers_clean.write.mode("overwrite").parquet("hdfs://localhost/user/cloudera/clean_data/Project/Customers_parquet/")
 
 
-# Stop Spark session
+
 sc.stop()
